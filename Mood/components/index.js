ba01119f378ce607b204176317374694a0a604e0
PabloSanchezCamara <157015926+PabloSanchezCamara@users.noreply.github.com>
2025-08-04 18:47:02 +0200

    Create index.js

---
+++ Mood/components/index.js
@@ -0,0 +1,6 @@
+export { default as FaceMoods } from './FaceMoods/FaceMoods.container';
+export { default as MoodTextarea } from './MoodTextarea/MoodTextarea.view';
+export { default as Feedback } from './Feedback/Feedback.view';
+export { default as GifButton } from './GifButton/GifButton.view';
+export { default as Icon } from './Icon/Icon.container';
+export { default as GiphySearcher } from './GiphySearcher/GiphySearcher.container';
